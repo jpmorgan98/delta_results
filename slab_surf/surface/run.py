2a247c2bb386788e234b4902d170b1c2bb3e05b4
Joanna Piper Morgan <morgjack@oregonstate.edu>
2025-04-18 16:18:17 -0700

    adding runners

--- slab_surf/surface/run.py
+++ slab_surf/surface/run.py
@@ -10,20 +10,20 @@ ratio = np.array([1, 5, 10, 100])
 delta_tracking = False
 collision_est = False
 
-runtimes = np.zeros((ratio.size, surfaces.size))
-errors = np.zeros((ratio.size, surfaces.size))
+runtimes = np.zeros((ratio.size, surfaces.size)).astype(np.float64)
+errors = np.zeros((ratio.size, surfaces.size)).astype(np.float64)
 
 for i in range(ratio.size):
     for j in range(surfaces.size):
-        print("\nRunning {} surfaces and Σ2/Σ1 {}".format(surfaces[i], ratio[j]))
+        print("\nRunning {} surfaces and Σ2/Σ1 {}".format(surfaces[j], ratio[i]))
 
-        os.system("srun -n 112 python input.py {} {} {} {}".format(surfaces[i], ratio[j], delta_tracking, collision_est)
+        os.system("srun -n 112 python input.py {} {} {} {}".format(surfaces[j], ratio[i], delta_tracking, collision_est)
                 + " --no-progress_bar --mode=numba --caching")
 
         start = time.time()
 
-        os.system("srun -n 112 python input.py {} {} {} {}".format(surfaces[i], ratio[j], delta_tracking, collision_est)
-                + " --no-progress_bar --mode=numba --caching=False")
+        os.system("srun -n 112 python input.py {} {} {} {}".format(surfaces[j], ratio[i], delta_tracking, collision_est)
+                + " --no-progress_bar --mode=numba --caching")
         
         end = time.time()
         runtimes[i, j] = end-start
@@ -33,7 +33,7 @@ for i in range(ratio.size):
 
         errors[i,j] = np.linalg.norm(phi_sd)
 
-        print(" completed in {}, with a vairance {}, FOM: {}".format(runtimes[i, j], errors[i,j], 1/(runtimes[i, j], errors[i,j])))
+        print("  completed in {}, with a vairance {}".format(runtimes[i, j], errors[i,j]))
 
 np.savez('data.npz', surfaces = surfaces, ratio=ratio, delta_tracking=delta_tracking, collision_est=collision_est, runtimes=runtimes, errors=errors)
 
